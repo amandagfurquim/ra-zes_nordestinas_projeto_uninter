d357d4a373b6a98b04f7c5bee8ad74b58048438f
Amanda Furquim <161618504+amandagfurquim@users.noreply.github.com>
2026-06-26 16:01:30 -0300

    Add files via upload

--- database.py
+++ database.py
@@ -4,6 +4,13 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./raiz_nordestina.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+banco_engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=banco_engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
